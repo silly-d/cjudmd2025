d0ed21c35da668c963ea970ad7f6f9279283008d
silly-d <rnjsdyddn734@gmail.com>
2025-10-25 21:53:10 +0900

    Update: 1025 hero.js modify

--- js/hero.js
+++ js/hero.js
@@ -244,19 +244,26 @@ heroReveal.forEach((element) => {
   });
 });
 
+let isTransitioning = false;
+
 ScrollTrigger.create({
   trigger: ".content05",
-  start: "top top",
-  end: "bottom bottom",
-  onLeave: () => {
-    // 페이드 아웃 효과 후 이동
-    gsap.to("body", {
-      opacity: 0,
-      duration: 1,
-      ease: "power2.inOut",
-      onComplete: () => {
-        window.location.href = "./about/about.html";
-      },
-    });
+  start: "bottom 80%",
+  onEnter: () => {
+    if (!isTransitioning) {
+      isTransitioning = true;
+
+      // 페이드 아웃 후 about.html로 이동
+      gsap.to("body", {
+        opacity: 0,
+        duration: 0.8,
+        ease: "power2.inOut",
+        onComplete: () => {
+          // sessionStorage에 플래그 저장
+          sessionStorage.setItem("fromIndex", "true");
+          window.location.href = "./about/about.html";
+        },
+      });
+    }
   },
 });
