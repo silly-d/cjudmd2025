4b0dce8edc4b3f657c3f372abd25c6d02fcb4889
silly-d <cjudmd2018@gmail.com>
2025-11-17 21:31:16 +0900

    1117 작업

--- designer/js/designer.js
+++ designer/js/designer.js
@@ -1,20 +1,3 @@
-const lenis = new Lenis({
-  duration: 1.2,
-  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-});
-
-lenis.on("scroll", ScrollTrigger.update);
-
-gsap.ticker.add((time) => {
-  lenis.raf(time * 1000);
-});
-
-gsap.ticker.lagSmoothing(0);
-
-ScrollTrigger.defaults({
-  scroller: lenis.wrapper,
-});
-
 const isMobile = () => window.innerWidth <= 768;
 
 function getPositions() {
